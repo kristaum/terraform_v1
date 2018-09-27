5db5f60985c2bbf6e6bed068066ea21425c66c54
kristaum <christopher.kristao@gmail.com>
2018-09-27 03:28:12 -0300

    Corrected security_group so private instance could reach the internet and also access it via ssh

--- main.tf
+++ main.tf
@@ -93,12 +93,25 @@ resource "aws_security_group" "sample_sg_web" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   # outbound internet access
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["${aws_subnet.sample_private_subnet.cidr_block}"]
   }
 }
 
@@ -121,7 +134,14 @@ resource "aws_security_group" "sample_sg_db" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["${aws_vpc.sample_vpc.cidr_block}"]
+    security_groups = ["${aws_security_group.sample_sg_web.id}"]
+  }
+
+  ingress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   # outbound internet access for software updates
@@ -136,6 +156,13 @@ resource "aws_security_group" "sample_sg_db" {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = -1
+    to_port = -1
+    protocol = "icmp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
